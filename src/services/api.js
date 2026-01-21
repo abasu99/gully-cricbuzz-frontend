f7858030065064f80693288b9ae7c30e403e8b95
Arnab <asrbaban@gmail.com>
2026-01-21 23:01:33 +0530

    baseURL added correctly

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,16 @@
 import axios from "axios";
 
-// const vercelBaseURL = 'https://gully-cricbuzz-backend.vercel.app/';
+const vercelBaseURL = 'https://gully-cricbuzz-backend.vercel.app/';
 // const localBaseURL = 'http://localhost:3001/';
+
 const matchApi = axios.create({
-  matchBaseURL: "https://gully-cricbuzz-backend.vercel.app/api/v1/match",
+  baseURL: `${vercelBaseURL}api/v1/match`,
   headers: {
     'Authorization': `Bearer ${sessionStorage.getItem('token')}`
   }
 });
 const authApi = axios.create({
-  authBaseURL: "https://gully-cricbuzz-backend.vercel.app/api/v1/auth"
+  baseURL: `${vercelBaseURL}api/v1/auth`
 });
 
-export { matchApi, authApi };
+export {matchApi,authApi};
