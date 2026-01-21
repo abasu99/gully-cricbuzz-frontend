e659eb94643b08ff839f5ba828feaa8ae4ab23c0
Arnab <asrbaban@gmail.com>
2026-01-21 22:53:05 +0530

    url changed

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,15 @@
 import axios from "axios";
 
-const vercelBaseURL = 'https://gully-cricbuzz-backend.vercel.app/';
+// const vercelBaseURL = 'https://gully-cricbuzz-backend.vercel.app/';
 // const localBaseURL = 'http://localhost:3001/';
-
 const matchApi = axios.create({
-  matchBaseURL: `${vercelBaseURL}api/v1/match`,
+  matchBaseURL: "https://gully-cricbuzz-backend.vercel.app/api/v1/match",
   headers: {
     'Authorization': `Bearer ${sessionStorage.getItem('token')}`
   }
 });
 const authApi = axios.create({
-  authBaseURL: `${vercelBaseURL}api/v1/auth`
+  authBaseURL: "https://gully-cricbuzz-backend.vercel.app/api/v1/auth"
 });
 
-export {matchApi,authApi};
+export { matchApi, authApi };
