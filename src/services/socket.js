873a74440730ef9c592fa866a6046244d662b81a
Arnab <asrbaban@gmail.com>
2026-01-21 22:41:13 +0530

    vercel url added in socket

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3001");
+// const socket = io("http://localhost:3001");
+const socket = io("https://gully-cricbuzz-backend.vercel.app");
 
 export default socket;
