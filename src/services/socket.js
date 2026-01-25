8674c533b54a6b0939402891bdbe4928b4247d37
Arnab <asrbaban@gmail.com>
2026-01-25 20:49:57 +0530

    transport added

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,11 +1,12 @@
 import { io } from "socket.io-client";
 
-// const SERVER_URL = "http://localhost:3002";
+// const SERVER_URL = "http://localhost:3001";
 const SERVER_URL = "https://gully-cricbuzz-backend.vercel.app";
 
 const socket =io(SERVER_URL, 
     {
-    path: "/score-path/"
+    path: "/score-path/",
+    transports: ["websocket"]
   }
 );
 
